a5a4c869d6e3251b00845166e6f8d15088109d17
Michael Wiessner <michael-wiessner@web.de>
2022-03-03 20:46:56 +0100

    guitarsController als resource eingebunden, links angepasst

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\GuitarsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +15,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [HomeController::class, 'about']);
-Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/about', [HomeController::class, 'about'])->name('home.about');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
+
+Route::resource('guitars', GuitarsController::class);
 
 Route::get('/return', [HomeController::class, 'return']);
 Route::get('/pipi', [HomeController::class, 'pipi']);
